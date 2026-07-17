ebd841f64d41c79fb1e942838e05b4cb080ad8ad
seifeddineba <sayf.Benaicha@trimakus.com>
2026-07-17 09:20:24 +0200

    remove rating

--- components/skills/skills-card.tsx
+++ components/skills/skills-card.tsx
@@ -1,4 +1,3 @@
-import Rating from "@/components/skills/rating";
 import { skillsInterface } from "@/config/skills";
 
 interface SkillsCardProps {
@@ -20,7 +19,6 @@ export default function SkillsCard({ skills }: SkillsCardProps) {
               <p className="text-sm text-muted-foreground">
                 {skill.description}
               </p>
-              <Rating stars={skill.rating} />
             </div>
           </div>
         </div>
